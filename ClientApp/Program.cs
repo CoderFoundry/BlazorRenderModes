9121a03bc4225eae41b647cd796f76e6d5817d20
Jacob Visick <jacobvisick@gmail.com>
2023-08-23 16:50:43 -0400

    Add project files.

---
+++ ClientApp/Program.cs
@@ -0,0 +1,9 @@
+using ClientApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
